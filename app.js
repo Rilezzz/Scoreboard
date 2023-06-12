7b6f06bc50a5e8e6d7f875226f7f196ac2e7f787
Riley <riley@idahosuperiorcustoms.com>
2023-06-12 17:03:28 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let homeScore = 0
+let awayScore = 0
+
+
+function increaseHomeScore() {
+  homeScore++
+  console.log('homeScore', homeScore);
+
+  run()
+}
+function increaseAwayScore() {
+  homeScore++
+  console.log('awayScore', awayScore);
+
+  run()
+}
+
+function run() {
+  document.getElementById('homeBeginScore').innerText = homeScore
+  document.getElementById('awayBeginScore').innerText = awayScore
+}
